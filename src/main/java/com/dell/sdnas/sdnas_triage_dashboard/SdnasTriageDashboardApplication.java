7e69e292c3cc78b42d67e62ef654cdfd884f6098
Gears <wmsgmp@gmail.com>
2023-01-08 13:32:55 +0530

    BlankRepo

---
+++ src/main/java/com/dell/sdnas/sdnas_triage_dashboard/SdnasTriageDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.dell.sdnas.sdnas_triage_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdnasTriageDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdnasTriageDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dell/sdnas/sdnas_triage_dashboard/SdnasTriageDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dell.sdnas.sdnas_triage_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdnasTriageDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
